d1669b6da7d61be765e1951c6e5539e867549a68
PauloMurakami <ppmura13@gmail.com>
2022-07-30 23:02:09 -0300

    Adicionando Deletar Categoria

--- src/services/DeleteCategoryService.ts
+++ src/services/DeleteCategoryService.ts
@@ -1,10 +1,14 @@
 import { AppDataSource } from "../db/dataSource";
 import { Category } from "../entities/Category";
+import { Post, Route, Tags, Body, Response, Request, Delete } from "tsoa";
 
 
-
+@Route("categories")
+@Tags("Deletar Categoria")
 export class DeleteCategorySerivce {
-    async execute(id: string) {
+    @Delete("{id}")
+    @Response<Error>(500, "Erro interno do servidor")
+    async execute(@Request() id: string) {
         const categoryRepository = AppDataSource.getRepository(Category);
         if(!(await categoryRepository.findBy({id: id}))){
             throw new Error("Category not found");
